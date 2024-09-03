01e0c72c3f5ead08661fea5b651a641282a883d9
oldSorcerer <aleksei.derevyannykh@gmail.com>
2024-09-03 14:51:33 +0300

    create constants class

---
+++ homework-03-collections-map/src/main/java/com/company/Constants.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public interface Constants {
+
+    String ENTER_INFO = "Введите номер, имя или команду: ";
+    String NO_SUCH_NAME = "Такого имени в телефонной книге нет.";
+    String ENTER_THE_NUMBER_FOR_CONTACT = "Введите номер телефона для абонента “%s”: ";
+    String CONTACT_SAVED = "Контакт сохранен!";
+    String ENTER_NAME_FOR_NUMBER = "Введите имя абонента для номера “%s”:";
+    String INCORRECT_FORMAT = "Неверный формат ввода";
+    String LIST = "LIST";
+
+}
