c8b99ab90255613361975a6bc7a443faf4849382
oldSorcerer <aleksei.derevyannykh@gmail.com>
2024-09-03 14:53:46 +0300

    add point to star

--- homework-03-collections-map/src/main/java/com/company/Main.java
+++ homework-03-collections-map/src/main/java/com/company/Main.java
@@ -2,6 +2,7 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-
+        PhoneBookApp phoneBookApp = new PhoneBookApp();
+        phoneBookApp.init();
     }
 }
