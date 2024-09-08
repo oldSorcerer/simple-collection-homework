4cedd3a3ff7f8a0aea510f34fa1d8566966f6e80
oldSorcerer <aleksei.derevyannykh@gmail.com>
2024-09-08 16:40:58 +0300

    create constants class

---
+++ homework-02-collections-set/src/main/java/com/company/Constants.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public interface Constants {
+
+    String EXIT = "EXIT";
+    String ADD = "ADD ";
+    String LIST = "LIST";
+    String WRONG_EMAIL_ANSWER = "Неверный формат email";
+
+}
