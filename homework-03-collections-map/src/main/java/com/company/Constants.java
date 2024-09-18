0b2feacfaae1d42ded4f699276a7e262f601574c
oldSorcerer <aleksei.derevyannykh@gmail.com>
2024-09-18 19:15:31 +0300

    update constants class

--- homework-03-collections-map/src/main/java/com/company/Constants.java
+++ homework-03-collections-map/src/main/java/com/company/Constants.java
@@ -6,6 +6,7 @@ public interface Constants {
     String NO_SUCH_NAME = "Такого имени в телефонной книге нет.";
     String ENTER_THE_NUMBER_FOR_CONTACT = "Введите номер телефона для абонента “%s”: ";
     String CONTACT_SAVED = "Контакт сохранен!";
+    String NO_SUCH_PHONE = "Такого номера нет в телефонной книге.";
     String ENTER_NAME_FOR_NUMBER = "Введите имя абонента для номера “%s”:";
     String INCORRECT_FORMAT = "Неверный формат ввода";
     String LIST = "LIST";
