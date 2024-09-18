9c6cc328c2c72fe37187b6a565f5864b3abec376
oldSorcerer <aleksei.derevyannykh@gmail.com>
2024-09-18 19:15:08 +0300

    create constants class

---
+++ homework-04-collections-search/src/main/java/com/company/Constants.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public interface Constants {
+
+    String ENTER_INFO = "Введите номер для поиска: ";
+    String PATTERN = "Поиск %s: номер '%s'%s найден, поиск занял %d нс%n";
+}
