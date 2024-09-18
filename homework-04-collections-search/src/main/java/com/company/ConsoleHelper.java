a1bccf220def2fb3dae4145167102f5f82b58449
oldSorcerer <aleksei.derevyannykh@gmail.com>
2024-09-18 19:14:40 +0300

    create helper class

---
+++ homework-04-collections-search/src/main/java/com/company/ConsoleHelper.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import lombok.SneakyThrows;
+import lombok.experimental.UtilityClass;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+@UtilityClass
+public class ConsoleHelper {
+
+    private static final BufferedReader CONSOLE = new BufferedReader(new InputStreamReader(System.in));
+
+    public void writeMessage(String message) {
+        System.out.print(message);
+    }
+
+    @SneakyThrows
+    public String readString() {
+        return CONSOLE.readLine();
+    }
+}
